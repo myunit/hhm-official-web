a3f3c34aa0a27238eb563ec25a0dce6aa53c8e53
joneqian <qian.qing@aliyun.com>
2016-05-10 09:54:43 +0800

    需求修改

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: '首页-好好卖' });
+  res.render('index', { title: '首页 - 好好卖' });
+});
+
+router.get('/investment', function(req, res, next) {
+  res.render('investment', { title: '招商 - 好好卖' });
 });
 
 module.exports = router;
